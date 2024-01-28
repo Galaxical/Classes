3882b3911f6b56138d552c58e067fd49b5186cf4
codeClass <azisomtochukwu@gmail.com>
2024-01-28 13:19:22 +0100

    counter and greet oupput

--- class.js
+++ class.js
@@ -1,5 +1,16 @@
 class count{
     static counter = 0; //create static property for count
+
+    //add a constructor that counts 
+    constructor(){
+        count.counter ++ //increment counter on instances
+    }
+
+    static greet(){
+        console.log("Hello, good evening!") 
+    }
 }
 
-console.log(count.counter)
+
+console.log(count.counter) //output the counting
+count.greet();//output the greet instance
