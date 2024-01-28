38711e61eab83d446155d0707b153468eba4cded
Descriptive Stats <azisomtochukwu@gmail.com>
2024-01-28 21:06:04 +0100

    mode

--- class.js
+++ class.js
@@ -1,11 +1,12 @@
 class descriptiveStatistics{
+    
     //mean data
+    static mean(data) {
+    const sum = data.reduce((acc, value) => acc + value, 0);
+    return sum / data.length;
+  }
 
-    static mean(data){
-        const sum = data.reduce((acc, value) => acc + value, 0)
-        return sum/data.length;
-    }
-
+    //Median
     static median (data){
         const sortedData = data.sort((a, b) => a -b);
         const middle = Math.floor(sortedData.length/2)
@@ -18,8 +19,30 @@ class descriptiveStatistics{
             return sortedData[middle]
         }
     }
+
+    //Mode
+    static mode(data){
+        const highFrequency = {}
+        data.foreach(value => {
+            highFrequency[value] = (highFrequency[value] || 0) + 1;
+        });
+
+        let mode;
+        let maxFrequency = 0;
+
+        for(const key in highFrequency){
+            if(highFrequency[key] > maxFrequency){
+                mode = key;
+                maxFrequency = highFrequency[key];
+            }
+        }
+
+        return mode;
+    }
+
 }
 
 const data = [12, 5, 17, 22, 13, 19, 10, 32, 27]
 console.log(descriptiveStatistics.mean())
 console.log(descriptiveStatistics.median())
+console.log(descriptiveStatistics.mode())
